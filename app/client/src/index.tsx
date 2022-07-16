4474314afb3d46634514bd5f71d3da70767ef7f3
ri4an <vadimguber7@gmail.com>
2022-07-16 17:10:52 +0600

    Dev: change arh

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/App/ui'
+import App from './components/App/ui'
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <App />
