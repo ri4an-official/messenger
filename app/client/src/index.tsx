30dcb45db148ec8ef34cfb35870148fed1fad8f0
ri4an <vadimguber7@gmail.com>
2022-07-16 17:13:52 +0600

    Dev: rename dirs

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App/ui'
+import App from './ui/components/App/ui'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
